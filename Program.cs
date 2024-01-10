a056ada532653b6fb46eb84ee7b537b042d296c6
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:21:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19__Class_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1= new Student("VISION",56,23);
+            Student st2 = new Student("VISION", 66, 23);
+
+            Console.WriteLine("is obese true or false : " + st1.isObese());
+            Console.WriteLine("is obese true or false : " + st2.isObese());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19__Class_Methods
+{
+    internal class Student
+    {
+        double weight ;
+        string name ;
+        int age ;
+
+        public Student(string name, double weight,  int age)
+        {
+            this.weight = weight;
+            this.name = name;
+            this.age = age;
+        }
+
+       public bool isObese()
+        {
+            if(weight>60) {
+            return true;
+            }
+            else return false;
+        }
+    }
+}
